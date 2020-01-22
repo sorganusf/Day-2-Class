8e2470dd7bbf11ccaab80c5f733eaa24d3e75132
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-01-21 20:41:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day_2_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Sean's message for the class.");
+            Console.ReadLine();
+        }
+    }
+}
